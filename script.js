f123fd1f59d9fb88c9d4a9490105875af6f04859
ciut1st <williamyeoshuhok1st@gmail.com>
2023-10-21 19:29:30 +1100

    done for now

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
-const first = "wes"
-let second = "bos"
-var age = 100;
+const Gameboard = (() => {
+    let gameboard = ["", "", "", "", "", "", "", "", "",]
 
-console.log(first)
-console.log(second)
-console.log(age)
+    const render = () => {
+        let boardHTML = ""
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id="square-${index}">${square}</div>`
+        })
+        document.querySelector('.gameboard').innerHTML = boardHTML
+    }
+    
+
+    return {
+        render
+    }
+})()
+
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    }
+}
+
+const Game = (() => {
+    let players = []
+    let currentPlayerIndex
+    let gameOver
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector('#player1').value, "X"),
+            createPlayer(document.querySelector('#player2').value, "O")
+        ]
+        currentPlayerIndex = 0
+        gameOver = false
+        Gameboard.render()
+    }
+    return {
+        start
+    }
+})()
+
+const startButton = document.querySelector('#start-button')
+startButton.addEventListener('click', () => {
+    Game.start()
+})
+
+const restartButton = document.querySelector('#restart-button')
+restartButton.addEventListener('click', () => {
+    alert('Restart')
+})
