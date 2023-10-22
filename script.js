841471cf660cc272176ced878a3b75abe014e5ff
ciut1st <williamyeoshuhok1st@gmail.com>
2023-10-22 13:37:13 +1100

    done for now

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const displayController = (() => {
+    const renderMessage = (message) => {
+        document.querySelector("#message").innerHTML = message
+    }
+    return {
+        renderMessage
+    }
+})()
+
 const Gameboard = (() => {
     let gameboard = ["", "", "", "", "", "", "", "", "",]
 
@@ -13,9 +22,17 @@ const Gameboard = (() => {
         })
     }
     
+    const update = (index, value) => {
+        gameboard[index] = value
+        render()
+    }
+
+    const getGameboard = () => gameboard
 
     return {
-        render
+        render,
+        update,
+        getGameboard
     }
 })()
 
@@ -42,14 +59,64 @@ const Game = (() => {
     }
 
     const handleClick = (event) => {
-        console.log(event.target.id);
+        if (gameOver) {
+            return
+        }
+        let index = parseInt(event.target.id.split("-")[1])
+        if (Gameboard.getGameboard()[index] !== "")
+            return
+
+        Gameboard.update(index, players[currentPlayerIndex].mark)
+
+        if (checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].mark)) {
+            gameOver = true
+            displayController.renderMessage(`${players[currentPlayerIndex].name} wins!`)
+        } else if (checkForTie(Gameboard.getGameboard())) {
+            gameOver = true
+            displayController.renderMessage("It is a tie!")
+        }
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0
+    }
+
+    const restart = () => {
+        for (let i = 0; i < 9; i++) {
+            Gameboard.update(i, "")
+        }
+        Gameboard.render()
+        gameOver = false
+        document.querySelector("#message").innerHTML = ""
     }
 
     return {
         start,
+        restart,
         handleClick
     }
 })()
+
+function checkForWin(board) {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    for (let i = 0; i < winningCombinations.length; i++) {
+        const [a, b, c] = winningCombinations[i]
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            return true
+        }
+    }
+    return false
+}
+
+function checkForTie(board) {
+    return board.every(cell => cell !== "")
+}
 
 const startButton = document.querySelector('#start-button')
 startButton.addEventListener('click', () => {
@@ -58,5 +125,5 @@ startButton.addEventListener('click', () => {
 
 const restartButton = document.querySelector('#restart-button')
 restartButton.addEventListener('click', () => {
-    alert('Restart')
+    Game.restart()
 })
